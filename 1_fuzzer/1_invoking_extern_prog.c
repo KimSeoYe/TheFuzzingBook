c5e8ef44a998d50677f2aa3b16d9dbe79d69d50a
KimSeoYe <47961698+KimSeoYe@users.noreply.github.com>
2021-08-04 14:24:35 +0900

    Update 1_invoking_extern_prog.c

--- 1_fuzzer/1_invoking_extern_prog.c
+++ 1_fuzzer/1_invoking_extern_prog.c
@@ -11,7 +11,7 @@ void
 execute_command (char * program, char * path)
 {
     char * command = (char *) malloc(sizeof(char) * (strlen(path) + 32)) ;  // TODO. use local variable 
-    sprintf(command, "cat %s | %s", path, program) ;    // TODO. modify -> "bc <file path>" -> how to check error?
+    sprintf(command, "cat %s | %s", path, program) ;    // TODO. modify -> "bc <file path>" -> how to check & count error?
 
     FILE * fp = popen(command, "r") ;
     if (fp == 0x0) {
