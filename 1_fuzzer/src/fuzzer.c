a95f2add16d67d683d5a51e448b46ecbe3befa12
KimSeoYe <21800088@handong.edu>
2021-08-05 12:27:52 +0900

    Collect the index of stdout and stderr execution

--- 1_fuzzer/src/fuzzer.c
+++ 1_fuzzer/src/fuzzer.c
@@ -11,14 +11,13 @@
 char *
 fuzzer (int max_length, int char_start, int char_range)
 {
-    // srand(clock()) ;    // Q.
-
     int string_length = rand() % (max_length + 1) ;
     char * out = (char *) malloc(sizeof(char) * string_length) ;    // Q. Is it okay to use malloc() here?
     for (int i = 0; i < string_length; i++) {
         out[i] = rand() % char_range + char_start ;
     }
-    out[string_length] = 0x0 ;
+    // Q. need to add 0x0 ?
+
 #ifdef DEBUG
     printf("fuzzer: %s\n", out) ;
 #endif
--- 1_fuzzer/test/1_invoking_extern_prog.c
+++ 1_fuzzer/test/1_invoking_extern_prog.c
@@ -8,26 +8,27 @@
 #include "../include/fuzzer.h"
 #include "../include/fileio.h"
 
-#define DEBUG
+// #define DEBUG
 
 int pipes[2] ;
-int error_pipes[2] ;
+int stderr_pipes[2] ;
 
 void
 child_proc (char * program, char * path)
 {
     close(pipes[0]) ;
-    close(error_pipes[0]) ;
+    close(stderr_pipes[0]) ;
+
     // dup2(pipes[1], 0) ; // ERROR (standard_in) 1: read() in flex scanner failed
     int dev_null = open("/dev/null", O_CLOEXEC) ;    // Q. ... close on exec
     dup2(dev_null, 0) ;
     dup2(pipes[1], 1) ;
-    dup2(error_pipes[1], 2) ;
+    dup2(stderr_pipes[1], 2) ;
 
     execlp(program, program, path, 0x0) ;
 }
 
-void
+int
 parent_proc (char * dir_name, int i) 
 {
     int exit_code ;
@@ -38,7 +39,7 @@ parent_proc (char * dir_name, int i)
 #endif
 
     close(pipes[1]) ;
-    close(error_pipes[1]) ;
+    close(stderr_pipes[1]) ;
 
     char out_path[32] ;
     sprintf(out_path, "%s/%s%d", dir_name, "output", i) ;
@@ -46,35 +47,39 @@ parent_proc (char * dir_name, int i)
     FILE * fp = fopen(out_path, "wb") ;
     if (fp == 0x0) {
         perror("fopen") ;
-        return ;
+        exit(1) ;
     }   
 
-    // Q. possible to get results from both stdout and stderr ?
-    // Q. can i choose a pipe ...?
+    // Q. possible to get results from both stderr and stdout ? : >> if the fuzzer string has \n in the middle?
+
     // int w = fwrite(&exit_code, 1, sizeof(exit_code), fp) ;   // TODO. use it later
+    // TODO. stderr중에 exit code가 0이 아닌 것(ex. 127)에 대해 따로 어떤 일을 해 주어야 할 것 같다.
     char header[32] ;
     sprintf(header, "%d\n", exit_code) ;
     int w = fwrite(header, 1, strlen(header), fp) ;
 
+    int flag = 1 ;  // treat empty input as a normal
+
     char buf[1024] ;
-    int s ;
+    int s = 0 ;
     while ((s = read(pipes[0], buf, 1024)) > 0) {
-        if ((w = fwrite(buf, 1, s, fp)) != s) {
+        if ((w = fwrite(buf, 1, s, fp)) < s) {
             perror("fwrite") ;
         }
 	}
     close(pipes[0]) ;
 
-    while ((s = read(error_pipes[0], buf, 1024)) > 0) {
-        if ((w = fwrite(buf, 1, s, fp)) != s) {
+    while ((s = read(stderr_pipes[0], buf, 1024)) > 0) {
+        flag = 2 ;
+        if ((w = fwrite(buf, 1, s, fp)) < s) {
             perror("fwrite") ;
         }
 	}
-    close(error_pipes[0]) ;
+    close(stderr_pipes[0]) ;
 
     fclose(fp) ;
 
-    return ;
+    return flag ;
 }
 
 int
@@ -84,25 +89,26 @@ my_popen (char * program, char * dir_name, char * in_path, int i)
         perror("pipe") ;
         exit(1) ;
     }
-    if (pipe(error_pipes) != 0) {
+    if (pipe(stderr_pipes) != 0) {
         perror("error pipe") ;
         exit(1) ;
     }
+
+    int flag ;
 
     pid_t child_pid = fork() ;
     if (child_pid == 0) {
         child_proc(program, in_path) ;
     }
     else if (child_pid > 0) {
-        parent_proc(dir_name, i) ;
+        flag = parent_proc(dir_name, i) ;
     }
     else {
         perror("fork") ;
-        return -1 ;
+        exit(1) ;
     }
 
-
-    return 1 ;
+    return flag ;
 }
 
 void
@@ -117,12 +123,6 @@ invoking_extern_prog ()
 
     if (write_data(path, "2 + 2\n") == -1)  return ;
     my_popen(program, dir_name, path, 0) ;
-    
-    if (remove(path) == -1) {
-        perror("remove") ;
-        exit(1) ;
-    }
-    // remove_input_dir(dir_name) ;
 }
 
 void
@@ -137,13 +137,40 @@ long_running_fuzzing ()
     char in_path[32] ;
 
     // Q. 100 input files and 100 output files..?
+
+    int stdout_num = 0 ;
+    int stderr_num = 0 ;
+    int stdout_results[100] ; 
+    int stderr_results[100] ;
+
     for (int i = 0; i < trials; i++) {
         sprintf(in_path, "%s/%s%d", dir_name, "input", i) ;
         char * data = fuzzer(MAX_LEN, CHAR_START, CHAR_RANGE) ;
-        if (write_data(in_path, data) == -1)  return ;
-        int exit_code = my_popen(program, dir_name, in_path, i) ;   // TODO. something with error code
+        data = realloc(data, strlen(data) + 1) ;
+        data[strlen(data)] = '\n' ;
+
+        if (write_data(in_path, data) == -1)  {
+            exit(1) ;
+        }
         free(data) ;
+
+        int flag = my_popen(program, dir_name, in_path, i) ;
+        if (flag == 1) {
+            stdout_results[stdout_num] = i ;
+            stdout_num++ ;
+        }
+        else if (flag == 2) { 
+            stderr_results[stderr_num] = i ;
+            stderr_num++ ;
+        }
     }
+    printf("STDOUT: %d\nSTDERR: %d\n", stdout_num, stderr_num) ;
+#ifdef DEBUG
+    printf("STDOUT executions: \n") ;
+    for (int j = 0; j < stdout_num; j++) {
+        printf("%s/output%d\n", dir_name, stdout_results[j]) ;
+    }
+#endif
 }
 
 int
