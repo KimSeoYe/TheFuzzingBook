3145c982b6e71bd7232d138e7d06a19c16de04fb
KimSeoYe <21800088@handong.edu>
2021-08-05 18:11:39 +0900

    Modify fuzzer to add null character

--- 1_fuzzer/src/fuzzer.c
+++ 1_fuzzer/src/fuzzer.c
@@ -12,11 +12,11 @@ char *
 fuzzer (int max_length, int char_start, int char_range)
 {
     int string_length = rand() % (max_length + 1) ;
-    char * out = (char *) malloc(sizeof(char) * string_length) ;    // Q. Is it okay to use malloc() here?
+    char * out = (char *) malloc(sizeof(char) * string_length + 1) ;    // Q. Is it okay to use malloc() here?
     for (int i = 0; i < string_length; i++) {
         out[i] = rand() % char_range + char_start ;
     }
-    // Q. need to add \0 ?
+    out[string_length] = '\0' ;
 
 #ifdef DEBUG
     printf("fuzzer: %s\n", out) ;
