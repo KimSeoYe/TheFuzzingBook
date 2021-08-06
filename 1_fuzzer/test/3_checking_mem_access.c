ca519bae82f762897a4ce8c23e8bf448f3ce5c35
KimSeoYe <21800088@handong.edu>
2021-08-06 11:14:18 +0900

    Add comments to 3_checking_mem_access.c

--- 1_fuzzer/test/3_checking_mem_access.c
+++ 1_fuzzer/test/3_checking_mem_access.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Compile : clang -fsanitize=address -g -o program program.c
+
 int main(int argc, char** argv) {
     /* Create an array with 100 bytes, initialized with 42 */
     char *buf = malloc(100);
@@ -14,3 +16,14 @@ int main(int argc, char** argv) {
     free(buf);
     return val;
 }
+
+
+/**
+ * HeartBleed Bug
+ * 
+ * in OpenSSL library
+ * if the client send the service a string like BIRd (500 letters), the server send BIRD + more payload, which can contains some secret data.
+ * 
+ * OpenSSL library를 memory sanitizer와 함께 compile 
+ * >> fuzzer command로 test를 했을 때, out-of-bounds memory access를 확인하게 됨으로써 발견되었다.
+*/
