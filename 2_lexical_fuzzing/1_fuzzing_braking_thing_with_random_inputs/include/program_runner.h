f88dfc182ee74fd3f5fb7218c0ddc4f6c89c41b6
KimSeoYe <21800088@handong.edu>
2021-08-09 17:09:06 +0900

    Add ifndef PROG_RUNNER

--- 2_lexical_fuzzing/1_fuzzing_braking_thing_with_random_inputs/include/program_runner.h
+++ 2_lexical_fuzzing/1_fuzzing_braking_thing_with_random_inputs/include/program_runner.h
@@ -1,3 +1,6 @@
+#ifndef PROG_RUNNER
+#define PROG_RUNNER
+
 enum pr_outs { PR_PASS = 0, PR_FAIL, PR_UNRESOLVED } ;
 
 typedef struct pr_ret_t {
@@ -6,3 +9,5 @@ typedef struct pr_ret_t {
 } pr_ret_t ;
 
 void program_runner_run (char * program, char * inp, pr_ret_t * ret) ;
+
+#endif
