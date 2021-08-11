84b6d5246907eb25452199eb3ec8a683b15b5389
KimSeoYe <21800088@handong.edu>
2021-08-11 12:16:10 +0900

    Complete testing PrintRunner

--- 2_lexical_fuzzing/1_fuzzing_braking_thing_with_random_inputs/fuzzing_architecture/test/runner_test.c
+++ 2_lexical_fuzzing/1_fuzzing_braking_thing_with_random_inputs/fuzzing_architecture/test/runner_test.c
@@ -7,13 +7,17 @@
 void
 print_runner_test ()
 {
-    Runner print_runner ;
+    Runner print_runner ;   // naming...
     PrintRunnerInit(&print_runner) ;
     
     char input[] = "Hello\0 World\n" ;
-    print_runner.run(&print_runner.result, input, sizeof(input)) ;
-    if (print_runner.result.input != 0x0)
-        free(print_runner.result.input) ;
+    print_runner.run(&print_runner, input, sizeof(input)) ;
+    
+    for (int i = 0; i < print_runner.result.input_size; i++) {
+        putc(print_runner.result.input[i], stdout) ;
+    }
+
+    print_runner.free_input(print_runner) ;
 }
 
 int
