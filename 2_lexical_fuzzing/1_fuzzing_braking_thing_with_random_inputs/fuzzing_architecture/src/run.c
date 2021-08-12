de0feecc0d5146c4f6949081e2a92e0ad0b3da17
KimSeoYe <21800088@handong.edu>
2021-08-12 13:52:01 +0900

    Implement the structure of run()

--- 2_lexical_fuzzing/1_fuzzing_braking_thing_with_random_inputs/fuzzing_architecture/src/run.c
+++ 2_lexical_fuzzing/1_fuzzing_braking_thing_with_random_inputs/fuzzing_architecture/src/run.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "../include/run.h"
 
 void 
+execute_binary(char * input, int * stdin_pipes, int * stdout_pipes, int * stderr_pipes)
+{
+    
+}
+
+void
+save_results(char * input, int * stdin_pipes, int * stdout_pipes, int * stderr_pipes)
+{
+
+}
+
+void 
 run (test_config_t * config, char * input)
 {
-    printf("RUN!\n") ;
+    int stdin_pipes[2] ;
+    int stdout_pipes[2] ;
+    int stderr_pipes[2] ;
+
+    if (pipe(stdin_pipes) != 0) {
+        perror("pipe") ;
+        exit(1) ;
+    }
+    if (pipe(stdout_pipes) != 0) {
+        perror("pipe") ;
+        exit(1) ;
+    }
+    if (pipe(stderr_pipes) != 0) {
+        perror("pipe") ;
+        exit(1) ;
+    }
+
+    pid_t child_pid = fork() ;
+    if (child_pid == 0) {
+        execute_binary(input, stdin_pipes, stdout_pipes, stderr_pipes) ;
+    }
+    else if (child_pid > 0) {
+        save_results(input, stdin_pipes, stdout_pipes, stderr_pipes) ;
+    }
+    else {
+        perror("fork") ;
+        exit(1) ;
+    }
 }
