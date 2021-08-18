0d662c20febc1c7545f84c1c09e529576de599c5
KimSeoYe <kimseoye15@gmail.com>
2021-08-18 09:54:11 +0900

    Rename testing files

---
+++ 2_lexical_fuzzing/2_code_coverage/test/whitebox_testing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * White-Box Testing : derive tests from the implementation.
+ * 
+ * Coverage criteria
+ * 1. Statement coverage
+ * 2. Branch coverage
+*/
+
+int
+main (int argc, char * argv[])
+{
+    if (argc != 2) {
+        perror("white_box_testing: usage: ") ;
+    }
+}
