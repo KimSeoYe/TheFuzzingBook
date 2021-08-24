706fff35f2774d18868661978f29a4e201c23126
KimSeoYe <kimseoye15@gmail.com>
2021-08-24 13:00:21 +0900

    Implement simple ver. of http_program

--- 2_lexical_fuzzing/3_mutation_based_fuzzing/practice/lib/urlparse.c
+++ 2_lexical_fuzzing/3_mutation_based_fuzzing/practice/lib/urlparse.c
@@ -15,7 +15,7 @@ typedef struct url {
 } url_t ;
 
 int
-urlparse (url_t * result, char * input)
+urlparse (url_t * result, char * url)
 {
     regex_t preg ;
     char * pattern = "^([a-z]+)://([a-z0-9.]+)/?([a-z0-9./]*)\\?\?([a-z0-9=]*)#?([a-z]*)" ;
@@ -24,39 +24,53 @@ urlparse (url_t * result, char * input)
 
     if (regcomp(&preg, pattern, REG_EXTENDED) != 0) {
         perror("urlparse: regcomp") ;
-        exit(1) ;
+        return -1 ;
     }
 
-    if (regexec(&preg, input, nmatch, pmatch, 0) != 0) {
-        perror("urlparse: regexec") ;
-        exit(1) ;
+    if (regexec(&preg, url, nmatch, pmatch, 0) != 0) {
+        perror("urlparse: regexec: Invalid url") ;
+        return -1 ;
     }
 
-    printf("%s\n", input + pmatch[0].rm_so) ;
+    printf("%s\n", url + pmatch[0].rm_so) ;
 
-    memcpy(result->scheme, input + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so) ;
+    memcpy(result->scheme, url + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so) ;
     result->scheme[pmatch[1].rm_eo - pmatch[1].rm_so] = 0x0 ;
 
-    memcpy(result->netloc, input + pmatch[2].rm_so, pmatch[2].rm_eo - pmatch[2].rm_so) ;
+    memcpy(result->netloc, url + pmatch[2].rm_so, pmatch[2].rm_eo - pmatch[2].rm_so) ;
     result->netloc[pmatch[2].rm_eo - pmatch[2].rm_so] = 0x0 ;
 
-    memcpy(result->path, input + pmatch[3].rm_so, pmatch[3].rm_eo - pmatch[3].rm_so) ;
+    memcpy(result->path, url + pmatch[3].rm_so, pmatch[3].rm_eo - pmatch[3].rm_so) ;
     result->path[pmatch[3].rm_eo - pmatch[3].rm_so] = 0x0 ;
 
-    memcpy(result->query, input + pmatch[4].rm_so, pmatch[4].rm_eo - pmatch[4].rm_so) ;
+    memcpy(result->query, url + pmatch[4].rm_so, pmatch[4].rm_eo - pmatch[4].rm_so) ;
     result->query[pmatch[4].rm_eo - pmatch[4].rm_so] = 0x0 ;
 
-    memcpy(result->fragment, input + pmatch[5].rm_so, pmatch[5].rm_eo - pmatch[5].rm_so) ;
+    memcpy(result->fragment, url + pmatch[5].rm_so, pmatch[5].rm_eo - pmatch[5].rm_so) ;
     result->fragment[pmatch[5].rm_eo - pmatch[5].rm_so] = 0x0 ;
 
     return 0 ;
 }
 
 int
+http_program (char * url)
+{
+    // TODO. supported_schemes = ["http", "https"]
+    url_t result ;
+    urlparse(&result, url) ;
+    if (strcmp(result.scheme, "http") != 0 && strcmp(result.scheme, "https") != 0) {
+        perror("http_program: scheme must be one of http, https") ;
+        exit(1) ;
+    }
+    
+    return 1 ;
+}
+
+int
 main ()
 {
     url_t result ;
-    urlparse(&result, "https://www.naver.com/path?query#frag") ;
+    urlparse(&result, "https://www.naver.com/path?q=query#frag") ;
     printf("%s\n%s\n%s\n%s\n%s\n", result.scheme, result.netloc, result.path, result.query, result.fragment) ;
 
     return 0 ;
