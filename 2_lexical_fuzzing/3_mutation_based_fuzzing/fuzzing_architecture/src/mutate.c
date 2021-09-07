98f7bc3dbf784fb63d164339ce3863ee4fb9549a
KimSeoYe <kimseoye15@gmail.com>
2021-09-08 03:11:25 +0900

    Implement simple_arithmatic and known_integers
    mutate() function now uses an array of fucntion pointers

--- 2_lexical_fuzzing/3_mutation_based_fuzzing/fuzzing_architecture/src/mutate.c
+++ 2_lexical_fuzzing/3_mutation_based_fuzzing/fuzzing_architecture/src/mutate.c
@@ -7,9 +7,47 @@
 #include "../include/mutate.h"
 #include "../include/fuzzer.h"
 
-// #define DEBUG
+#define DEBUG
 
-typedef enum mutator { DELETE = 0, INSERT, FLIP } mutator_t ;
+#define INTERESTING_8 \
+  -128,          /* Overflow signed 8-bit when decremented  */ \
+  -1,            /*                                         */ \
+   0,            /*                                         */ \
+   1,            /*                                         */ \
+   16,           /* One-off with common buffer size         */ \
+   32,           /* One-off with common buffer size         */ \
+   64,           /* One-off with common buffer size         */ \
+   100,          /* One-off with common buffer size         */ \
+   127           /* Overflow signed 8-bit when incremented  */
+
+#define INTERESTING_16 \
+  -32768,        /* Overflow signed 16-bit when decremented */ \
+  -129,          /* Overflow signed 8-bit                   */ \
+   128,          /* Overflow signed 8-bit                   */ \
+   255,          /* Overflow unsig 8-bit when incremented   */ \
+   256,          /* Overflow unsig 8-bit                    */ \
+   512,          /* One-off with common buffer size         */ \
+   1000,         /* One-off with common buffer size         */ \
+   1024,         /* One-off with common buffer size         */ \
+   4096,         /* One-off with common buffer size         */ \
+   32767         /* Overflow signed 16-bit when incremented */
+
+#define INTERESTING_32 \
+  -2147483648LL, /* Overflow signed 32-bit when decremented */ \
+  -100663046,    /* Large negative number (endian-agnostic) */ \
+  -32769,        /* Overflow signed 16-bit                  */ \
+   32768,        /* Overflow signed 16-bit                  */ \
+   65535,        /* Overflow unsig 16-bit when incremented  */ \
+   65536,        /* Overflow unsig 16 bit                   */ \
+   100663045,    /* Large positive number (endian-agnostic) */ \
+   2147483647    /* Overflow signed 32-bit when incremented */
+
+static u_int8_t  interesting_8[] = { INTERESTING_8 } ;
+static u_int16_t interesting_16[] = { INTERESTING_8, INTERESTING_16 } ;
+static u_int32_t interesting_32[] = { INTERESTING_8, INTERESTING_16, INTERESTING_32 } ;
+
+typedef enum mutator { DELETE = 0, INSERT, BIT_FLIP, BYTE_FLIP, ARITHMATIC, KNOWN_INT } mutator_t ;
+
 
 int
 delete_random_character (char * dst, char * seed, int seed_len) 
@@ -58,11 +96,11 @@ insert_random_character (char * dst, char * seed, int seed_len)
 }
 
 int
-flip_random_character (char * dst, char * seed, int seed_len) 
+bit_flip (char * dst, char * seed, int seed_len) 
 {
     if (seed_len == 0) {
         dst[0] = 0x0 ;
-        perror("flip_random_character: seed_len == 0") ;
+        perror("bit_flip: seed_len == 0") ;
         return 0 ;
     } 
 
@@ -71,7 +109,7 @@ flip_random_character (char * dst, char * seed, int seed_len)
     char new_char = (char) seed[position] ^ bit ;   // Q.
 
 #ifdef DEBUG
-    printf("Flipping %d in %d giving %c at %d\n", bit, seed[position], new_char, position) ;
+    printf("Bit-flip %d in %d giving %c at %d\n", bit, seed[position], new_char, position) ;
 #endif
 
     memcpy(dst, seed, position) ;
@@ -84,22 +122,108 @@ flip_random_character (char * dst, char * seed, int seed_len)
 }
 
 int
-mutate (char * dst, char * seed, int seed_len)
+byte_flip (char * dst, char * seed, int seed_len) 
 {
-    int mutator = rand() % 3 ;
-    int new_len = 0 ;
+    if (seed_len == 0) {
+        dst[0] = 0x0 ;
+        perror("byte_flip: seed_len == 0") ;
+        return 0 ;
+    } 
 
-    switch (mutator) {
-        case DELETE: 
-            new_len = delete_random_character(dst, seed, seed_len) ;
+    int position = rand() % seed_len ;
+    int bit = 1 << (rand() % 7) ;
+    char new_char = (char) seed[position] ^ bit ;   // Q.
+
+#ifdef DEBUG
+    printf("Bit-flip %d in %d giving %c at %d\n", bit, seed[position], new_char, position) ;
+#endif
+
+    memcpy(dst, seed, position) ;
+    dst[position] = new_char ;
+
+    memcpy(dst + position + 1, seed + position + 1, seed_len - position - 1) ;
+    dst[seed_len] = 0x0 ;
+
+    return seed_len ;
+}
+
+int
+simple_arithmatic (char * dst, char * seed, int seed_len) 
+{
+    if (seed_len == 0) {
+        dst[0] = 0x0 ;
+        perror("simple_arithmatic: seed_len == 0") ;
+        return 0 ;
+    } 
+
+    int position = rand() % seed_len ;
+    int add_operand = rand() % 71 - 35 ;
+    char new_char = (char) seed[position] + add_operand ;
+
+#ifdef DEBUG
+    printf("Simple arithmatic: seed[position]=%d, new_char=%c, position=%d\n", seed[position], new_char, position) ;
+#endif
+
+    memcpy(dst, seed, position) ;
+    dst[position] = new_char ;
+
+    memcpy(dst + position + 1, seed + position + 1, seed_len - position - 1) ;
+    dst[seed_len] = 0x0 ;
+
+    return seed_len ;
+}
+
+int
+known_integers (char * dst, char * seed, int seed_len) 
+{
+    if (seed_len == 0) {
+        dst[0] = 0x0 ;
+        perror("known_integers: seed_len == 0") ;
+        return 0 ;
+    } 
+
+    int known_integers_num[] = { 9, 10, 8 } ;
+
+    int position = rand() % seed_len ;
+
+    int category = rand() % 3 ;
+    int index = rand() % known_integers_num[category] ;
+    char new_char ;
+
+    switch (category) {
+        case 0:
+            new_char = interesting_8[index] ;
             break ;
-        case INSERT:
-            new_len = insert_random_character(dst, seed, seed_len) ;
+        case 1:
+            new_char = interesting_16[index] ;
             break ;
-        case FLIP:
-            new_len = flip_random_character(dst, seed, seed_len) ;
+        case 2:
+            new_char = interesting_32[index] ;
             break ;
     }
+
+#ifdef DEBUG
+    printf("Known integer: seed[position]=%d, new_char=%c, position=%d\n", seed[position], new_char, position) ;
+#endif
+
+    memcpy(dst, seed, position) ;
+    dst[position] = new_char ;
+
+    memcpy(dst + position + 1, seed + position + 1, seed_len - position - 1) ;
+    dst[seed_len] = 0x0 ;
+
+    return seed_len ;
+}
+
+int
+mutate (char * dst, char * seed, int seed_len) 
+{
+    int (* mutator[6]) (char *, char *, int) = { delete_random_character, insert_random_character, bit_flip, byte_flip, simple_arithmatic, known_integers } ;
+
+    int mutator_idx = rand() % 6 ;
+    int new_len = 0 ;
+
+    mutator[mutator_idx](dst, seed, seed_len) ; 
 
     return new_len ;
 }
