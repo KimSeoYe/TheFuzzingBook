460cc2dac1c0687be3a169b2c4d6b9a39d3e39ff
KimSeoYe <kimseoye15@gmail.com>
2021-09-08 16:17:03 +0900

    Add mutate_test.c

---
+++ 2_lexical_fuzzing/3_mutation_based_fuzzing/fuzzing_architecture/test/mutate_test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "../include/mutate.h"
+
+int
+main ()
+{
+    srand(time(NULL)) ;
+
+    char * seed = "Hello World!\n" ;
+    char dst[32] ;
+    
+    delete_random_character(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+
+    insert_random_character(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+
+    bit_flip(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+
+    byte_flip(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+
+    simple_arithmatic(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+
+    known_integers(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+
+    mutate(dst, seed, strlen(seed)) ;
+    printf(">> %s\n", dst) ;
+}
