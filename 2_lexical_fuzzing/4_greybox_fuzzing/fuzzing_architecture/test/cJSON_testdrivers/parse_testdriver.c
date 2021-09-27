a06dda7085b350ca9b2bc8a32c9f3afe897b1700
KimSeoYe <kimseoye15@gmail.com>
2021-09-28 02:18:25 +0900

    Modify parse_testdriver to single source version

--- 2_lexical_fuzzing/4_greybox_fuzzing/fuzzing_architecture/test/cJSON_testdrivers/parse_testdriver.c
+++ 2_lexical_fuzzing/4_greybox_fuzzing/fuzzing_architecture/test/cJSON_testdrivers/parse_testdriver.c
@@ -9,8 +9,8 @@ void
 set_configs (test_config_t * config)
 {
     strcpy(config->runargs.binary_path, "../../lib/cJSON/parse_test") ;
-    char sources[2][PATH_MAX] = { "../../lib/cJSON/parse_test.c", "../../lib/cJSON/cJSON.c"} ;
-    config->covargs.source_num = 2 ;
+    char sources[1][PATH_MAX] = { "../../lib/cJSON/cJSON.c"} ;
+    config->covargs.source_num = 1 ;
     config->covargs.source_paths = (char **) malloc(sizeof(char *) * config->covargs.source_num) ;
     for (int i = 0; i < config->covargs.source_num; i++) {
         config->covargs.source_paths[i] = (char *) malloc(sizeof(char) * PATH_MAX) ;
