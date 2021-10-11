f15ec5a1dca84eeb9a93d7b03df996735a1f4021
KimSeoYe <kimseoye15@gmail.com>
2021-10-11 21:25:01 +0900

    Update testprograms

--- 2_lexical_fuzzing/4_greybox_fuzzing/fuzzing_architecture/lib/cJSON/testprograms/SY_json_test.c
+++ 2_lexical_fuzzing/4_greybox_fuzzing/fuzzing_architecture/lib/cJSON/testprograms/SY_json_test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 #include <cjson/cJSON.h>
 
@@ -81,29 +83,6 @@ parse_and_print_with_opt (char * input)
     free(printed) ;
 
     cJSON_Delete(parsed_input_w_null_terminated) ;
-
-
-    cJSON * parsed_input_wo_null_terminated = cJSON_ParseWithOpts(input, 0x0, 0) ; 
-    if (parsed_input_wo_null_terminated == 0x0) {
-        perror("parse_and_print_with_opt: cJSON_ParseWithOpts failed") ;
-        return ;
-    }
-
-    printf("\n=========================================\n") ;
-    printf("PARSE_W/O_NULL_TERM: FORMATTED\n") ;
-    printf("=========================================\n") ;
-    printed = cJSON_Print(parsed_input_wo_null_terminated) ;
-    printf("%s\n", printed) ;
-    free(printed) ;
-
-    printf("\n=========================================\n") ;
-    printf("PARSE_W/O_NULL_TERM: UNFORMATTED\n") ;
-    printf("=========================================\n") ;
-    printed = cJSON_PrintUnformatted(parsed_input_wo_null_terminated) ;
-    printf("%s\n", printed) ;
-    free(printed) ;
-
-    cJSON_Delete(parsed_input_wo_null_terminated) ;
 }
 
 void
@@ -130,29 +109,6 @@ parse_and_print_with_length_opt (char * input, int input_len)
     free(printed) ;
 
     cJSON_Delete(parsed_input_w_null_terminated) ;
-
-
-    cJSON * parsed_input_wo_null_terminated = cJSON_ParseWithLengthOpts(input, input_len, 0x0, 0) ;
-    if (parsed_input_wo_null_terminated == 0x0) {
-        perror("parse_and_print_with_length_opt: cJSON_ParseWithLengthOpts failed") ;
-        return ;
-    } 
-
-    printf("\n=========================================\n") ;
-    printf("PARSE_LEN_W/O_NULL_TERM: FORMATTED\n") ;
-    printf("=========================================\n") ;
-    printed = cJSON_PrintBuffered(parsed_input_wo_null_terminated, input_len, 1) ;
-    printf("%s\n", printed) ;
-    free(printed) ;
-
-    printf("\n=========================================\n") ;
-    printf("PARSE_LEN_W/O_NULL_TERM: UNFORMATTED\n") ;
-    printf("=========================================\n") ;
-    printed = cJSON_PrintBuffered(parsed_input_wo_null_terminated, input_len, 0) ;
-    printf("%s\n", printed) ;
-    free(printed) ;
-
-    cJSON_Delete(parsed_input_wo_null_terminated) ;
 }
 
 void
@@ -170,8 +126,14 @@ parse_and_print_preallocated (char * input, int input_len)
     printf("PRINT_PREALLOCATED: FORMATTED\n") ;
     printf("=========================================\n") ;
     int printed = cJSON_PrintPreallocated(parsed_input, buf, input_len + 5, 1) ;
-    printf("%d\n%s", printed, buf) ;
-    free(buf) ;
+    if (printed) {
+        printf("%d\n%s", printed, buf) ;
+        free(buf) ;
+    }
+    else {
+        perror("parse_and_print_preallocated: cJSON_PrintPreallocated failed") ;
+    }
+    
 
     buf = (char *) malloc(sizeof(char) * (input_len + 5)) ;
 
@@ -179,10 +141,61 @@ parse_and_print_preallocated (char * input, int input_len)
     printf("PRINT_PREALLOCATED: UNFORMATTED\n") ;
     printf("=========================================\n") ;
     printed = cJSON_PrintPreallocated(parsed_input, buf, input_len + 5, 0) ;
-    printf("%d\n%s", printed, buf) ;
-    free(buf) ;
+    if (printed) {
+        printf("%d\n%s", printed, buf) ;
+        free(buf) ;
+    }
+    else {
+        perror("parse_and_print_preallocated: cJSON_PrintPreallocated failed") ;
+    }
 
     cJSON_Delete(parsed_input) ;  
+}
+
+void
+duplicate_and_compare (char * input) 
+{
+    cJSON * parsed_input = cJSON_Parse(input) ;
+    if (parsed_input == 0x0) {
+        perror("parse_and_print_preallocated: cJSON_ParseWithLength failed") ;
+        return ;
+    }
+
+    cJSON * duplicated = cJSON_Duplicate(parsed_input, 1) ;
+    if (parsed_input == 0x0) {
+        perror("parse_and_print_preallocated: cJSON_Duplicate failed") ;
+        return ;
+    }
+    cJSON_bool is_same = cJSON_Compare(parsed_input, duplicated, 1);
+    if (is_same) printf("duplicate success\n") ;
+    else printf("duplicate failed\n") ;
+
+    cJSON_Delete(parsed_input) ;
+    cJSON_Delete(duplicated) ;
+}
+
+void
+parse_and_minify (char * input, int input_len)
+{
+    cJSON * parsed_input = cJSON_Parse(input) ;
+    if (parsed_input == 0x0) {
+        perror("parse_and_print_preallocated: cJSON_ParseWithLength failed") ;
+        return ;
+    }
+
+    char * copied = (char *) malloc(sizeof(char) * (input_len + 1)) ;
+    memcpy(copied, input, input_len + 1) ;
+
+    cJSON_Minify(copied) ;
+    if (copied != 0x0) {
+        printf("\n=========================================\n") ;
+        printf("MINIFIED\n") ;
+        printf("=========================================\n") ;
+        printf("%s\n", copied) ;
+    }
+
+    free(copied) ;
+    cJSON_Delete(parsed_input) ;
 }
 
 int
@@ -208,6 +221,9 @@ main (int argc, char * argv[])
     parse_and_print_with_opt(input) ;
     parse_and_print_with_length_opt(input, input_len) ;
     parse_and_print_preallocated(input, input_len) ;
+
+    duplicate_and_compare(input) ;
+    parse_and_minify(input, input_len) ;
 
     return 0 ;
 }
