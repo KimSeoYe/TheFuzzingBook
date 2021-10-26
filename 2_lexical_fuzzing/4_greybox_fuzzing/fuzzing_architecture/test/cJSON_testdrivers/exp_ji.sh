0ad59a519201559299ef6d91fd18dc0e3996631e
KimSeoYe <kimseoye15@gmail.com>
2021-10-26 23:39:11 +0900

    Update scripts for JI, SY

--- 2_lexical_fuzzing/4_greybox_fuzzing/fuzzing_architecture/test/cJSON_testdrivers/exp_ji.sh
+++ 2_lexical_fuzzing/4_greybox_fuzzing/fuzzing_architecture/test/cJSON_testdrivers/exp_ji.sh
@@ -4,25 +4,45 @@ make testdriver_JI
 
 trial=60000
 
+mut_updated_seeds=(0 0 0 0 0)
+
 mut_start=$(date +%s)
 for i in 1 2 3 4 5
 do  
    ./testdriver_JI -m -t $trial
+   mut_updated_seeds[${i}]=$(ls -l ./testprogram_seeds/seed_JI/mutated_* | wc -l)
+   rm ./testprogram_seeds/seed_JI/mutated_*
 done
 mut_end=$(date +%s)
+
+grey_updated_seeds=(0 0 0 0 0)
 
 grey_start=$(date +%s)
 for i in 1 2 3 4 5
 do  
    ./testdriver_JI -g -t $trial
+   grey_updated_seeds[${i}]=$(ls -l ./testprogram_seeds/seed_JI/mutated_* | wc -l)
    rm ./testprogram_seeds/seed_JI/mutated_*
 done
 grey_end=$(date +%s)
 
 echo ""
 echo "======================================================="
-echo "TOTAL TIME"
-echo "======================================================="
-echo "RANDOM MUTATION: $(($mut_end-$mut_start))sec"
-echo "GREYBOX: $(($grey_end-$grey_start))sec"
-echo ""
+echo "RANDOM MUTATION"
+echo "-------------------------------------------------------"
+echo "TIME: $(($mut_end-$mut_start))sec"
+printf "%s" "UPDATED SEEDS: "
+for i in 1 2 3 4 5
+do
+   printf "%d " "${mut_updated_seeds[i]}"
+done
+echo "\n======================================================="
+echo "GREYBOX"
+echo "-------------------------------------------------------"
+echo "TIME: $(($grey_end-$grey_start))sec"
+printf "%s" "UPDATED SEEDS: "
+for i in 1 2 3 4 5
+do
+   printf "%d " "${grey_updated_seeds[i]}"
+done
+echo "\n=======================================================\n"
